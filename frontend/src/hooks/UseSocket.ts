b0234b4c8a1bead1c3c952993684f1fd6b45a317
akshathere <akshatverma86825@gmail.com>
2024-11-13 23:27:38 +0530

    changed websocket server from local to prod

--- frontend/src/hooks/UseSocket.ts
+++ frontend/src/hooks/UseSocket.ts
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 // Connect to WebSocket server
 const token = localStorage.getItem("token");
-const ws_local="ws://localhost:8080"
-// const ws_prod="wss://superficial-kindhearted-sense.glitch.me"
-const WS_URL = (`${ws_local}/?token=${token}`);
+// const ws_local="ws://localhost:8080"
+const ws_prod="wss://superficial-kindhearted-sense.glitch.me"
+const WS_URL = (`${ws_prod}/?token=${token}`);
 
 export default function useSocket() {
   const [socket, setSocket] = useState<WebSocket | null>(null);
